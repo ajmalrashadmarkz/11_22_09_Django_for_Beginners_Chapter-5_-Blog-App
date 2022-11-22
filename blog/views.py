cb4419987c9cf42b79e54f7e9ac2395eb044604b
rashadmarkz <rashad.markcreative.com>
2022-11-22 14:22:33 +0530

    9. Setup the template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
+from django.views.generic import ListView
+from .models import Post
 # Create your views here.
-class HomePageView(TemplateView):
+
+class HomePageView(ListView):
+    model = Post
     template_name = "home.html"
